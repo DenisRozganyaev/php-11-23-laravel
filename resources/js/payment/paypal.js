a58ffa7cc19c85bacf2dccdae181fefacae6f405
apple <apple@MacBook-Pro-apple.local>
2024-03-19 19:03:04 +0200

    test

--- resources/js/payment/paypal.js
+++ resources/js/payment/paypal.js
@@ -40,7 +40,7 @@ paypal.Buttons({
 
     // Call your server to set up the transaction
     createOrder: function (data, actions) {
-        return axios.post(window.baseUrl + '/ajax/paypal/order/create/', getFields())
+        return axios.post(window.baseUrl + '/ajax/paypal/order/create', getFields())
             .then(function (res) {
                 return res.data.vendor_order_id
             })
@@ -49,7 +49,7 @@ paypal.Buttons({
     // Call your server to finalize the transaction
     onApprove: function (data, actions) {
         console.log('data', data)
-        return axios.post(window.baseUrl + `/ajax/paypal/order/${data.orderID}/capture/`)
+        return axios.post(window.baseUrl + `/ajax/paypal/order/${data.orderID}/capture`)
             .then(function (res) {
                 return res.data;
             }).then(function (orderData) {
